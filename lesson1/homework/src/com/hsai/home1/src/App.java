0fdb4ce1c3e84c436cf6780970ef86c7044b8a7a
Benhooky <78452165+Benhooky@users.noreply.github.com>
2022-09-12 22:58:38 +0300

    Update App.java

--- lesson1/homework/src/com/hsai/home1/src/App.java
+++ lesson1/homework/src/com/hsai/home1/src/App.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         Scanner in = new Scanner(System.in);
         var BaseArray = IOUtils.toIntArray(in.nextLine().split(""));
         var SortNumber = BaseArray[0];
