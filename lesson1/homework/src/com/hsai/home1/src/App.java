bd73f4dd21660530945ce9b8d5e5fe14f5ed6c8d
Benhooky <egorkulygin444@gmail.com>
2022-09-14 01:36:44 +0300

    Formating and true style classes names

--- lesson1/homework/src/com/hsai/home1/src/App.java
+++ lesson1/homework/src/com/hsai/home1/src/App.java
@@ -1,39 +1,44 @@
-import inOut.IOUtils;
+import alghorithm.InsertSort;
 import alghorithm.MergeSort;
 import alghorithm.QuickSort;
 import alghorithm.SelecrionSort;
-import alghorithm.insert_Sort;
-
+import inOut.IOUtils;
 import java.util.Arrays;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        var base_array = IOUtils.toIntArray(args);
-        var sort_number = base_array[0];
-        int[] аrr_for_sort = Arrays.copyOfRange(base_array, 1, base_array.length);
-        while (sort_number<0){
-            sort_number+=4;
-        }
-        while (sort_number>3){
-            sort_number-=4;
-        }
-        switch (sort_number) {
-            case 0:
-                insert_Sort.insertionSort(аrr_for_sort);
-                break;
-            case 1:
-                SelecrionSort.SelecrionSort(аrr_for_sort);
-                break;
-            case 2:
-                MergeSort.MergeSort(аrr_for_sort, аrr_for_sort.length);
-                break;
-            case 3:
-                QuickSort.quickSort(аrr_for_sort, 0, аrr_for_sort.length - 1);
-                break;
-            default:
-                break;
-        }
-        String EasyMenu[] = { "Insert Sort", "Selecrion Sort", "Merge Sort", "Quick Sort" };
-        IOUtils.writeOutput(EasyMenu[sort_number], аrr_for_sort);
+
+  public static void main(String[] args) throws Exception {
+    var base_array = IOUtils.toIntArray(args);
+    var sort_number = base_array[0];
+    int[] аrr_for_sort = Arrays.copyOfRange(base_array, 1, base_array.length);
+    while (sort_number < 0) {
+      sort_number += 4;
     }
+    while (sort_number > 3) {
+      sort_number -= 4;
+    }
+    switch (sort_number) {
+      case 0:
+        InsertSort.InsertionSort(аrr_for_sort);
+        break;
+      case 1:
+        SelecrionSort.SelecrionSort(аrr_for_sort);
+        break;
+      case 2:
+        MergeSort.MergeSort(аrr_for_sort, аrr_for_sort.length);
+        break;
+      case 3:
+        QuickSort.QuickSort(аrr_for_sort, 0, аrr_for_sort.length - 1);
+        break;
+      default:
+        break;
+    }
+    String EasyMenu[] = {
+      "Insert Sort",
+      "Selecrion Sort",
+      "Merge Sort",
+      "Quick Sort",
+    };
+    IOUtils.writeOutput(EasyMenu[sort_number], аrr_for_sort);
+  }
 }
