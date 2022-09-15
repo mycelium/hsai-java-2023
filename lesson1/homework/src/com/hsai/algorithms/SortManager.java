37c1b4e330358db80b879194917d5c5f6a33140c
Adam <adamrashi@mail.ru>
2022-09-16 00:28:51 +0300

    basic code without jar

--- lesson1/homework/src/com/hsai/algorithms/SortManager.java
+++
@@ -1,9 +0,0 @@
-package lesson1.homework.src.com.hsai.algorithms;
-
-public class SortManager {
-    SortingAlgorithm sort;
-
-    public static void chooseSort() {
-
-    }
-}
--- lesson1/homework/src/com/hsai/algorithms/SortingAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package lesson1.homework.src.com.hsai.algorithms;
-
-import java.util.Collection;
-
-public static interface SortingAlgorithm {
-
-    static Integer[] sort(Integer[] sourceCollection);
-}
