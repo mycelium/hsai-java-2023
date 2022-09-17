0e4bc28e63b93eaa5406389acb8cae902dd3cd20
Sakralen <grigoriy.e.spasov@gmail.com>
2022-09-18 00:55:06 +0300

    minor fix

--- lesson1/homework/src/com/hsai/sortalgs/InsertionSort.java
+++ lesson1/homework/src/com/hsai/sortalgs/InsertionSort.java
@@ -1,7 +1,5 @@
 package com.hsai.sortalgs;
 
-import java.nio.file.attribute.GroupPrincipal;
-
 public class InsertionSort extends AbstractSort {
     public InsertionSort() {
         super("Insertion sort");
