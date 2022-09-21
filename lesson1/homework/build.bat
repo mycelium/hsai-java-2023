7b54759ee620b621b962448e532a6828938f159c
Alex <99346796+7am7ar@users.noreply.github.com>
2022-09-21 22:39:50 +0300

    Revert "First Task "

--- lesson1/homework/build.bat
+++
@@ -1,6 +0,0 @@
-javac -d .\bin .\src\com\hsai\*.java .\src\com\hsai\sort\*.java
-cd bin
-jar -cfe ..\sorter.jar lesson1/homework/src/com/hsai/Main .\lesson1\homework\src\com\hsai\*.class .\lesson1\homework\src\com\hsai\sort\*.class
-cd..
-rmdir /s /q bin
-cmd /k
