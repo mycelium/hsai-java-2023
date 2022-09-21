9995fbd149bffcae22ec2c6752cd3dd969845fb7
cookie237 <fedotov-staska.zenit@mail.ru>
2022-09-21 21:37:38 +0300

    Delete comment

--- lesson1/homework/src/com/hsai/algorithms/QuickSorting.java
+++ lesson1/homework/src/com/hsai/algorithms/QuickSorting.java
@@ -19,30 +19,22 @@ public class QuickSorting extends ParentSort {
         int leftMarker = leftBorder;
         int rightMarker = rightBorder;
         int pivot = array[(leftMarker + rightMarker) / 2];
-        do {
-            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
-            while (array[leftMarker] < pivot) {
+        do {while (array[leftMarker] < pivot) {
                 leftMarker++;
             }
-            // Двигаем правый маркер, пока элемент больше, чем pivot
             while (array[rightMarker] > pivot) {
                 rightMarker--;
             }
-            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
             if (leftMarker <= rightMarker) {
-                // Левый маркер будет меньше правого только если мы должны выполнить swap
                 if (leftMarker < rightMarker) {
                     int tmp = array[leftMarker];
                     array[leftMarker] = array[rightMarker];
                     array[rightMarker] = tmp;
                 }
-                // Сдвигаем маркеры, чтобы получить новые границы
                 leftMarker++;
                 rightMarker--;
             }
         } while (leftMarker <= rightMarker);
-
-        // Выполняем рекурсивно для частей
         if (leftMarker < rightBorder) {
             sort(array, leftMarker, rightBorder);
         }
