15b0920502c242d87e599a55fb3a8443158ed80f
Polina Glezova <pglezova@gmail.com>
2022-09-23 21:35:43 +0300

    Последняя попытка написать работающий скрипт.

--- lesson1/homework/my_skript.sh
+++ lesson1/homework/my_skript.sh
@@ -1,8 +1,7 @@
+#!/bin/sh
 rm -rf build
 mkdir build
-javac -d ./build /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/Main.java /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/IOUtils.java /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/sorts/*.java
-cd build || exit
-jar cfe sorter.jar lesson1/homework/src/com/hsai/Main lesson1/homework/src/com/hsai/Main.class lesson1/homework/src/com/hsai/sorts/*.class lesson1/homework/src/com/hsai/IOUtils.class
-cd ..
-#rm -rf build
+javac -d ./build src/com/hsai/Main.java src/com/hsai/IOUtils.java src/com/hsai/sorts/*.java
+cd build
+jar cfe ../sorter.jar lesson1/homework/src/com/hsai/Main lesson1/homework/src/com/hsai/Main.class lesson1/homework/src/com/hsai/sorts/*.class lesson1/homework/src/com/hsai/IOUtils.class
 
