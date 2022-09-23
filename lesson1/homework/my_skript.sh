28e7e2a6e2f133acbcdf6fee3a87f1f60f2d23c7
Polina Glezova <pglezova@gmail.com>
2022-09-23 14:11:17 +0300

    Работающий shell скрипт.

--- lesson1/homework/my_skript.sh
+++ lesson1/homework/my_skript.sh
@@ -1,7 +1,8 @@
+rm -rf build
 mkdir build
 javac -d ./build /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/Main.java /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/IOUtils.java /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/sorts/*.java
 cd build || exit
-jar cfe sorter.jar /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/main.class /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/sorts/*.class /Users/polinaglezova/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/IOUtils.class
+jar cfe sorter.jar lesson1/homework/src/com/hsai/Main lesson1/homework/src/com/hsai/Main.class lesson1/homework/src/com/hsai/sorts/*.class lesson1/homework/src/com/hsai/IOUtils.class
 cd ..
-rm -rf build
+#rm -rf build
 
