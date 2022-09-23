ea57ccaa0b11fe9ec0a0efc9989c9ee945ba8e9d
NastyaB <local.boor@yandex.ru>
2022-09-23 21:50:39 +0300

    Новая домашняя работа 1 с работающим скриптом

--- lesson1/homework/build.sh
+++ lesson1/homework/build.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 rm -rf build
 mkdir build
-javac -d ./build /Users/nnnn/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/Main.java /Users/nnnn/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/IOUtils.java /Users/nnnn/IdeaProjects/hsai-java-2023/lesson1/homework/src/com/hsai/sorts/*.java
+javac -d ./build src/com/hsai/Main.java src/com/hsai/IOUtils.java src/com/hsai/sorts/*.java
 cd build || exit
 # shellcheck disable=SC1001
-jar cfe sorter.jar lesson1/homework/src/com/hsai/Main lesson1/homework/src/com/hsai/sorts/*.class lesson1/homework/src/com/hsai/Main.class lesson1/homework/src/com/hsai/IOUtils.class
-cd ..
+jar cfe ../sorter.jar lesson1/homework/src/com/hsai/Main lesson1/homework/src/com/hsai/sorts/*.class lesson1/homework/src/com/hsai/Main.class lesson1/homework/src/com/hsai/IOUtils.class
+#cd ..
 #rm -rf build
