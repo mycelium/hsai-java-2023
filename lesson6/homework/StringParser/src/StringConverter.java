f718d05db87d5a4d663c4d932271a12430fc9490
fug0 <eleectricgore@gmail.com>
2022-11-10 23:18:55 +0300

    encoding conversion and output fixes

--- lesson6/homework/StringParser/src/StringConverter.java
+++ lesson6/homework/StringParser/src/StringConverter.java
@@ -1,18 +1,23 @@
 package lesson6.homework.StringParser.src;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.nio.charset.Charset;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class StringConverter {
-    static private final HashMap<String, Integer> nums = new HashMap<>();
+    static private final LinkedHashMap<String, Integer> nums = new LinkedHashMap<>();
+    static private final LinkedHashMap<String, String> codes = new LinkedHashMap<>();
     static private final String[] strNums = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать"};
     static private final Integer[] intNums = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    static private final String[] strCodes = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 
     static {
         for(int i = 0; i < intNums.length; i++) {
             nums.put(strNums[i], intNums[i]);
+            codes.put(strNums[i], strCodes[i]);
         }
     }
 
@@ -30,7 +35,7 @@ public class StringConverter {
         int corrNum = 0;
 
         for (int i = 0; i < byteStr.length(); i++) {
-            int digit = Integer.parseInt(String.valueOf(byteStr.charAt(byteStr.length() - i - 1)));
+            int digit = Integer.parseInt(String.valueOf(byteStr.charAt(byteStr.length() - i - 1)), base);
             if (digit >= base) return -1;
 
             corrNum += digit * (int) Math.pow(base, i);
@@ -41,26 +46,15 @@ public class StringConverter {
         return corrNum;
     }
     private int strToByte(String str) {
-        String strNumber = "";
-        String tempStr = "";
+        String[] keys = codes.keySet().toArray(new String[0]);
 
-        for(Character symb : str.toCharArray()) {
-            tempStr = tempStr.concat(symb.toString());
+        StringBuilder strNumber = new StringBuilder(str);
 
-            if (tempStr.equals("один") || tempStr.equals("три")) {
-                String longStr = "надцать";
-                if (str.length() >= tempStr.length() + longStr.length()) {
-                    if (str.substring(0, tempStr.length() + longStr.length()).equals(tempStr + longStr))
-                        tempStr = tempStr + longStr;
-                }
-            }
-            if (nums.containsKey(tempStr)) {
-                strNumber = strNumber.concat(nums.get(tempStr).toString());
-                tempStr = "";
-            }
+        for (int i = keys.length - 1; i >= 0; i--) {
+            strNumber = sbReplaceAll(strNumber, keys[i], codes.get(keys[i]));
         }
 
-        return correctBaseNumber(strNumber);
+        return correctBaseNumber(strNumber.toString());
     }
 
     public List<Byte> convertStrToByteArr() {
@@ -73,8 +67,18 @@ public class StringConverter {
 
         return convertedNumbers;
     }
+    private static StringBuilder sbReplaceAll(StringBuilder sb, String str1, String str2) {
+        StringBuilder res = new StringBuilder(sb);
+        int index = res.indexOf(str1);
 
-    public void printBytesArr(List<Byte> arr) {
+        while (index != -1) {
+            res.replace(index, index + str1.length(), str2);
+            index = res.indexOf(str1, index + str2.length());
+        }
+        return res;
+    }
+
+    public void printBytesArr(List<Byte> arr) throws IOException {
         byte[] bytesArray = new byte[arr.size()];
 
         for (int i = 0; i < arr.size(); i++) {
@@ -83,5 +87,11 @@ public class StringConverter {
 
         String stringToPrint = new String(bytesArray, Charset.forName(encoding));
         System.out.println(stringToPrint);
+
+        try(FileWriter fw = new FileWriter("output.txt", Charset.forName(encoding), false)) {
+            fw.write(stringToPrint);
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
     }
 }
