67914a5e437ffa73c3b078a20e21fd6255daf050
cookie237 <fedotov-staska.zenit@mail.ru>
2022-11-21 18:44:33 +0300

    second lab

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -10,42 +10,51 @@ object FunSets {
    * We represent a set by its characteristic function, i.e.
    * its `contains` predicate.
    */
-  type Set = Int => Boolean
+  type MySet = Int => Boolean
 
   /**
    * Indicates whether a set contains a given element.
    */
-  def contains(s: Set, elem: Int): Boolean = s(elem)
+  def contains(s: MySet, elem: Int): Boolean = s(elem)
 
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
-  
+    def singletonSet(elem: Int): MySet = k => k == elem
+
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
-  
+    def union(s: MySet, t: MySet): MySet = k => t match {
+      case Nil => s(k)
+      case _ => s(k) | t(k)
+    }
+
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
-  
+    def intersect(s: MySet, t: MySet): MySet = k => t match {
+      case Nil => s(k)
+      case _ => s(k) & t(k)
+    }
+
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
-  
+    def diff(s: MySet, t: MySet): MySet = k => t match {
+      case Nil => s(k)
+      case _ => s(k) & !t(k)
+    }
+
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+    def filter(s: MySet, p: Int => Boolean): MySet = k => s(k) & p(k)
+
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -55,30 +64,36 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+    def forall(s: MySet, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (!p(a) & contains(s,a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
-  
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+    def exists(s: MySet, p: Int => Boolean): Boolean = ! forall(s, k => !p(k))
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+    def map(s: MySet, f: Int => Int): MySet = {
+      def iter(a: Int): MySet = {
+        if (a > bound) Nil
+        else if (contains(s,a)) union(singletonSet(f(a)),iter(a + 1))
+        else iter(a + 1)
+      }
+      iter(-bound)
+    }
   /**
    * Displays the contents of a set
    */
-  def toString(s: Set): String = {
+  def toString(s: MySet): String = {
     val xs = for (i <- -bound to bound if contains(s, i)) yield i
     xs.mkString("{", ",", "}")
   }
@@ -86,7 +101,7 @@ object FunSets {
   /**
    * Prints the contents of a set on the console.
    */
-  def printSet(s: Set) {
+  def printSet(s: MySet) {
     println(toString(s))
   }
 }
