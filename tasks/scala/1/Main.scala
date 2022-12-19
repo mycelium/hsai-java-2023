a03ee58efa77d1f471e89efce49dd147ac5a9038
Polina Glezova <pglezova@gmail.com>
2022-12-19 13:20:24 +0300

    lab 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,4 +1,5 @@
 package recfun
+
 import common._
 
 object Main {
@@ -15,15 +16,35 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    c match {
+      case 0 => 1
+      case x if (c == r) => 1
+      case _ => pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def counts(chars: List[Char], count: Int): Boolean = {
+      if (count < 0) false
+      else if (chars.isEmpty) true
+      else chars.head match {
+        case ')' => counts(chars.tail, count - 1)
+        case '(' => counts(chars.tail, count + 1)
+        case _ => counts(chars.tail, count)
+      }
+    }
+
+    counts(chars, 0)
   }
+
+  println("Parentheses Balancing")
+  println(balance(List('(', ')')))
+  println(balance(List('(', ')', ')', '(', '(', ')')))
+  println(balance(List('(', '(', ')', '(', ')', ')')))
+
 
   /**
    * Exercise 3 Counting Change
@@ -33,6 +54,18 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    money match {
+      case 0 => 1
+      case negative if (money < 0) => 0
+      case empty if coins.isEmpty => 0
+      case _ => countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
   }
+
+  println("Counting Change")
+  println(countChange(0, List(1, 2)) == 1)
+  println(countChange(0, List()) == 1)
+  println(countChange(4, List()) == 0)
+  println(countChange(5, List(2, 3)) == 1)
+  println(countChange(4, List(1, 2)) == 3)
 }
