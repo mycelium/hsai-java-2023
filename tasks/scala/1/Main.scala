3ef01b854964db4349505c61d4531249816f5608
NastyaB <local.boor@yandex.ru>
2022-12-23 00:47:07 +0300

    Scala. Домашняя работа 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.collection.immutable.Range
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -15,16 +17,39 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    c match {
+      case negative if (r < 0 || c < 0) => 0
+      case 0 => 1
+      case _ => pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
+
+  println("Test for parentheses balancing")
+  println(pascal(1, 1))
+  println(pascal(2, 4))
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def count(chars: List[Char], countOpen: Int): Boolean = {
+      if (countOpen < 0) false
+      else if (chars.isEmpty) {
+        countOpen == 0
+        true
+      }
+      else {
+        if (chars.head == '(') count(chars.tail, countOpen + 1)
+        else if (chars.head == ')') count(chars.tail, countOpen - 1)
+        else count(chars.tail, countOpen)
+      }
+    }
+    count(chars, 0)
   }
 
+  println("Test for parentheses balancing")
+  println(balance(List('(', ')', ')', '(', '(', ')')))
+  println(balance(List('(', '(', ')', ')')))
   /**
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
@@ -33,6 +58,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) return 1
+    if ((money < 0) || (coins.isEmpty && money > 0)) return 0
+    countChange(money, coins.take(coins.length - 1)) + countChange(money - coins.last, coins)
   }
+
+  println("Test for counting change")
+  println(countChange(0, List(1, 2)) == 1)
 }
