e66db5dd37f7637275e14e736183c52e9ebded0c
azazuent <azazuent@gmail.com>
2022-12-28 12:57:31 +0300

    Caesar done

--- exam/azazuent/Base64Decoder/src/Main.java
+++ exam/azazuent/Base64Decoder/src/Main.java
@@ -1,7 +1,13 @@
 import decoder.Decoder;
 import decoder.Tester;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        Tester.Test();
+    public static void main(String[] args) throws IOException {
+        Tester.testBase64();
+        Tester.testCaesar();
+        Tester.solution();
     }
 }
